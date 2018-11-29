c01ad5244ed48112ba4a65ca7bcfa50939f4890e
Devin <lucky7devin@gmail.com>
2018-11-28 17:30:40 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ var player;
 function setup () {
   createCanvas(500,500);
 };
-
 function draw () {
-  background(50,50,50);
-  rect(100,80,300,499);
+  noStroke();
+  background(0,0,0);
+  fill(0,255,0);
+  ellipse(250,250,500,500);
+  fill(0,0,255);
+  ellipse(250,125,500,250);
+  fill(255,0,0);
+  ellipse(250,375,500,250);
+  fill(255,255,255);
+  ellipse(250,450,100,100);
+  ellipse(450,250,100,100);
+  ellipse(50,250,100,100);
+  ellipse(250,250,100,100);
+  ellipse(250,50,100,100);
+  fill(0);
+  ellipse(350,250,100,100);
+  ellipse(150,250,100,100);
+  ellipse(250,150,100,100);
+  ellipse(250,350,100,100);
+
 };
